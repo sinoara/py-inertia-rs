1296e7c0d464f6dabab1502d54053cac60037a23
Guilherme Sinoara <gsinoara@gmail.com>
2022-09-07 22:09:09 -0300

    removed example function

--- src/lib.rs
+++ src/lib.rs
@@ -82,17 +82,10 @@ impl PySystem {
     }
 }
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn py_inertia_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
     m.add_class::<PySystem>()?;
 
     Ok(())
